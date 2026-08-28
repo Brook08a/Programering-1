3b68fbe162e4e1aa6fb24fabd3ce739bdd4b4377
Beruk_Gebrekerestos <bbruk2719@gmail.com>
2026-08-28 13:26:34 +0200

    första uppgift

---
+++ uppgift1.py
@@ -0,0 +1,36 @@
+
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+first_name= input("skriv ditt namn: ")
+last_namn= input("skriv ditt efternamn: ")
+
+ 
+pi = 3.14159265
+
+r = 4
+
+print ( r * r * pi)
+
+
+
+Weight = float(input("vikt: "))
+
+
+
+
+
+
+
+
+
+
+
