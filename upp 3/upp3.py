94f227bc5e56cd5e24acb0be31dc49694594f671
Beruk_Gebrekerestos <bbruk2719@gmail.com>
2026-09-17 10:40:32 +0200

    Gjorde allt

---
+++ upp 3/upp3.py
@@ -0,0 +1,38 @@
+
+# height = input("Hur lång är du?: ")
+# try: 
+
+#     if int(height) < 140:
+#         print("You're not tall enough")
+
+#     elif int(height) > 140:
+#         print("You're tall enough")
+
+# except: 
+#     print("Try to type in your height")
+
+
+
+# try:
+#     first_name= input("skriv ditt namn: ")
+#     last_namn= input("skriv ditt efternamn: ")
+
+#     age = int(input("skriv din ålder: "))
+
+# except:
+#     print("Skriv din ålder")
+
+
+
+# try:
+#     vikt = float(input("Ange din vikt i Kg: "))
+#     Längd = float(input("Ange din längd i meter: "))
+#     bmi = vikt / (Längd ** 2)
+#     print("Ditt BMI är: " + str(bmi))
+
+# except:
+#     print(" BARA SIFFROR TILLÅTNA")
+
+
+
+
